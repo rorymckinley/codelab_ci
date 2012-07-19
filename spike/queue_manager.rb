d1835c66697ff6cb5f4b52b745f4cf57feb4e240
Rory McKinley <rorymckinley@gmail.com>
2012-07-19 22:58:26 +0200

    Spike logic for finding the latest request

--- spike/queue_manager.rb
+++ spike/queue_manager.rb
@@ -9,10 +9,26 @@ workbenches.reject! { |workbench| [".", ".."].include? workbench }
 workbenches.map! do |workbench|
   workbench_requests = Dir.entries(File.join(workbench_dir, workbench))
   workbench_requests.reject! { |workbench| [".", ".."].include? workbench }
-  request_times = workbench_requests.map { |request| File.ctime(File.join(workbench_dir, workbench, request)) }
+  request_times = workbench_requests.map { |request| File.mtime(File.join(workbench_dir, workbench, request)) }
   { :workbench => workbench, :earliest_request => request_times.sort.first }
 end
 
 workbenches.sort! { |x,y| x[:earliest_request] <=> y[:earliest_request] }
 
-p workbenches
+# Assume we only have one worker - no forking for now
+
+workbench = workbenches.first[:workbench]
+
+requests = Dir.entries(File.join(workbench_dir, workbench))
+
+requests.reject! { |request| [".", ".."].include? request }
+
+requests.sort! do |first_request, second_request|
+  File.mtime(File.join(workbench_dir, workbench, first_request)) <=> File.mtime(File.join(workbench_dir, workbench, second_request))
+end
+
+committer = IO.read(File.join(workbench_dir, workbench, requests.first)).chomp
+
+requests.select! { |request| IO.read(File.join(workbench_dir, workbench, request)).chomp == committer }
+
+puts requests.last
