b82afc2dc9eb56eda25d33131f3ed17cdfbcf618
Sheldon Hearn <sheldonh@starjuice.net>
2012-07-28 12:20:58 +0200

    Respike toward non-blocking testability

--- spike/imaginary-cronjob-controller.rb
+++ spike/imaginary-cronjob-controller.rb
@@ -1,6 +1,8 @@
-while request = scheduler.next_integration_request(dispatcher.busy_filter)
+until dispatcher.empty? and scheduler.empty?
   time_elapsed = time_operation do
-    dispatcher.start_integrating(request)
+    wait_until { !dispatcher.full? }
+    request = scheduler.next_integration_request(dispatcher.busy_filter)
+    dispatcher.start_integrating(request) if request
     dispatcher.completed_integrations.each do |integration|
       scheduler.complete_integration_request(integration.request)
       notifier.notify(integration)
@@ -8,7 +10,7 @@ while request = scheduler.next_integration_request(dispatcher.busy_filter)
   end
   if time_elapsed > x
     scheduler.read_queue
-  elsif time_elapsed < 1
-    sleep 1
+  elsif time_elapsed < 0.1
+    sleep 0.1
   end
 end
